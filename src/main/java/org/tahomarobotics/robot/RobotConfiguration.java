83442e94118ecfaaa187b0dfe35e5943c1dcce8d
ZH0PPE <97768900+zachhoppe@users.noreply.github.com>
2025-02-22 14:51:44 -0800

    Switched to Phoenix Pro (#135)
    Co-authored-by: nukkeldev <110308379+nukkeldev@users.noreply.github.com>

--- src/main/java/org/tahomarobotics/robot/RobotConfiguration.java
+++ src/main/java/org/tahomarobotics/robot/RobotConfiguration.java
@@ -8,7 +8,7 @@ public class RobotConfiguration {
     public static final File DEPLOY_DIR = Filesystem.getDeployDirectory();
 
     public static final String CANBUS_NAME = "CANivore";
-    public static final boolean CANIVORE_PHOENIX_PRO = false;
+    public static final boolean CANIVORE_PHOENIX_PRO = true;
     public static final boolean RIO_PHOENIX_PRO = false;
     public static final double ODOMETRY_UPDATE_FREQUENCY = 250;
     public static final double MECHANISM_UPDATE_FREQUENCY = 100;
