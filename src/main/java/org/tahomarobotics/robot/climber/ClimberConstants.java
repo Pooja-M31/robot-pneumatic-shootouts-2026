1ec63bca6818331815cede7a652554121ab4c3d0
Nicolas <130109338+niccsilv@users.noreply.github.com>
2025-03-20 18:17:17 -0700

    Climb Changes (#248)

--- src/main/java/org/tahomarobotics/robot/climber/ClimberConstants.java
+++ src/main/java/org/tahomarobotics/robot/climber/ClimberConstants.java
@@ -32,8 +32,8 @@ public class ClimberConstants {
     public static final double MOTOR_TO_CLIMBER_GEAR_RATIO = (8.0 / 72.0) * (18.0 / 72.0) * (16.0 / 48.0);
 
     public static final double ZERO_POSITION = 0.0;
-    public static final double STOW_POSITION = 0.267333984375;
-    public static final double DEPLOY_POSITION = 0.267333984375;
+    public static final double STOW_POSITION = 0.190673828125;
+    public static final double DEPLOY_POSITION = 0.190673828125;
     public static final double CLIMB_POSITION = -0.12;
 
     public static final double CLIMB_POSITION_TOLERANCE = 0.005;
